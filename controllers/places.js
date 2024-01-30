04f231d2b1fdae1409fdbfb97705568f4d50261c
Mandell1981 <Mandell1981.rm@gmail.com>
2024-01-30 16:07:22 -0500

    Added-the-edit-functionality

--- controllers/places.js
+++ controllers/places.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const places = require('../models/places.js')
+const places = require('../models/places')
 
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+// SHOW
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -20,7 +21,8 @@ router.get('/:id', (req, res) => {
   
 })
 
-router.get('/:id/edit', (req, res) => {
+// DELETE
+router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
     res.render('error404')
@@ -29,8 +31,39 @@ router.get('/:id/edit', (req, res) => {
     res.render('error404')
   }
   else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
+
+// EDIT
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else {
   res.render('places/edit', { place: places[id] })
   }
+})
+
+router.get('/', (req, res) => {
+  let places = [{
+      name: 'H-Thai-ML',
+      city: 'Seattle',
+      state: 'WA',
+      cuisines: 'Thai, Pan-Asian',
+      pic: '/images/H-Thai.jpg'
+      
+    }, {
+      name: 'Coding Cat Cafe',
+      city: 'Phoenix',
+      state: 'AZ',
+      cuisines: 'Coffee, Bakery',
+      pic: '/images/CodingCat-Cafe.jpg'
+    }]
+    
+  res.render('places/index', { places })
 })
 
 router.post('/', (req, res) => {
@@ -49,37 +82,33 @@ router.post('/', (req, res) => {
   res.redirect('/ places')
 })
 
-router.delete('/:id', (req, res) => {
+
+
+router.put('/:id', (req, res) => {
   let id = Number(req.params.id)
-  if (isNaN(id)) {
+  if (isNaN (id)) {
     res.render('error404')
   }
   else if (!places[id]) {
     res.render('error404')
   }
   else {
-    places.splice(id, 1)
-    res.redirect('/places')
+    // Dig into req.body and make sure data is valid
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    // Save the new data into places[id]
+    places[id] = req.body
+    res.redirect(`/places/${id}`)
   }
 })
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/H-Thai.jpg'
-        
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/CodingCat-Cafe.jpg'
-      }]
-      
-    res.render('places/index', { places })
-})
 
 module.exports = router
