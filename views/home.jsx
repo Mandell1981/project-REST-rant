a745b24f22610f4b1bf2107ad33871e307bde3df
Mandell1981 <Mandell1981.rm@gmail.com>
2024-02-13 17:03:45 -0500

    Playng-with-bootstrap

--- views/home.jsx
+++ views/home.jsx
@@ -6,8 +6,8 @@ function home () {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
-                <div>
+                <h1>Let's Eat</h1>
+                <div class="position-absolute top-50 start-50 translate-middle">
                     <img src="/images/noodles-bowl.jpg" alt="Ramen Bowl"/>
                      <div>
                      Photo by <a href="https://unsplash.com/@bonvivant?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Bon Vivant</a> on <a href="https://unsplash.com/photos/ramen-dish-qom5MPOER-I?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
